1ae8dbfc2a613148d2e7ec85ae75af50dfcff6f9
CoverProPainting <info@coverpropainting.com>
2026-06-17 17:32:13 -0700

    Update policies.js

--- routes/policies.js
+++ routes/policies.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { sendPolicyEmail } = require('../utils/email');
-const { generateMileageTrackingPDF } = require('../utils/pdf');
+const { generateMileageTrackingPDF, generateLaptopPolicyPDF } = require('../utils/pdf');
 
 router.get('/mileage-tracking', (req, res) => {
   res.render('policies/mileage-tracking');
@@ -17,4 +17,16 @@ router.post('/mileage-tracking', async (req, res) => {
   }
 });
 
-module.exports = router;
+router.get('/laptop-policy', (req, res) => {
+  res.render('policies/laptop-policy');
+});
+
+router.post('/laptop-policy', async (req, res) => {
+  try {
+    const pdfBuffer = await generateLaptopPolicyPDF(req.body);
+    await sendPolicyEmail('LAPTOP & ELECTRONIC DEVICE USAGE POLICY ACKNOWLEDGMENT', req.body, pdfBuffer);
+    res.render('policies/success', { policyName: 'Laptop & Electronic Device Usage Policy' });
+  } catch (error) {
+    res.render('policies/error', { error: error.message });
+  }
+});module.exports = router;
